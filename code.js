7d433e4ef4fdd1d80657a7e3576683f7a4ed0383
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-10-31 17:47:06 -0600

    Sub-array length calculation
    I think I've finally nailed down the handling of array lengths. But I guess the tests will let me know if I did or not.

--- code.js
+++ code.js
@@ -3,25 +3,20 @@ function divideAndConquerSum(a) {
     else if(a.length == 1) return a[0]; // single element, return single element.
     else if(a.length == 2) return a[0] + a[1]; // 2 elements, return sum of the two. 
 
-    var sum = split(a, 0, (a.length -1))
-    return sum;
+    return split(a, 0, (a.length -1));
 }
 
-// first third of array is 0 to subLen
-// second third of array is subLen to 2*subLen
-// third third of array is 2*subLen to len
-// each third should return the sum of the "subarrays" which at its lowest should each return only one value which can be summed with the others.
-// pass to split (array, low = 0, high = length-1)
+// 
 function split(arr, low, high) {
-    if(len == 1) return arr[low];
-    if((high - low) == 1) return arr[low] + arr[high];
-    var lowMid = Math.floor(((low+high)/2)/2);
-    var midMid = Math.floor((low+high)/2);
-    var highMid = Math.floor((midMid + high)/2);
+    if((high - low + 1) == 1) return arr[low];              // If there is only one element, return it.
+    if((high - low + 1) == 2) return arr[low] + arr[high];  // If two elements, sum and return.
+    var subLen = Math.floor((high-low+1)/3);                // Calculate length of 1/3 of the array.
+    var lowMid = low + subLen;                              // Calculate end point of first third.
+    var highMid = lowMid + subLen;                          // Calculate end point of second third.
 
-    var leftSum = split(arr, low, lowMid);
-    var midSum = split(arr, lowMid, highMid);
-    var rightSum = split(arr, highMid, high);
-    return leftSum + midSum + rightSum;
+    var leftSum = split(arr, low, lowMid);                  // Split first third
+    var midSum = split(arr, lowMid, highMid);               // Split second third
+    var rightSum = split(arr, highMid, high);               // Split last third
+    return leftSum + midSum + rightSum;                     // Return sum of each third.
 }
 // I really don't think this is going to work.
