12a7434f902f47f6ca22363b021359c80090cf1f
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-10-31 15:48:34 -0600

    Rough Draft Logic
    I honestly don't think this is going to work given the subLen handling but I want to see what fails in the tests.

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function divideAndConquerSum(a) {
-    return a;
+    if(a.length() == 0) return 0;   // empty, return 0.
+    else if(a.length() == 1) return a[0]; // single element, return single element.
+    else if(a.length() == 2) return a[0] + a[1]; // 2 elements, return sum of the two. 
+
+    var sum = split(a, 0, (a.length-1))
+    return sum;
 }
+
+// first third of array is 0 to subLen
+// second third of array is subLen to 2*subLen
+// third third of array is 2*subLen to len
+// each third should return the sum of the "subarrays" which at its lowest should each return only one value which can be summed with the others.
+// pass to split (array, low = 0, high = length-1)
+function split(arr, low, high) {
+    if(low == high) return arr[low];
+    if((high - low) == 1) return arr[low] + arr[high];
+    var subLen = Math.floor((low+high)/3); //
+
+    var leftSum = split(arr, low, subLen);
+    var midSum = split(arr, subLen, 2*subLen);
+    var rightSum = split(arr, 2*subLen, high);
+    return leftSum + midSum + rightSum;
+}
+// I really don't think this is going to work.
